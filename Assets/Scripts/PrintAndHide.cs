60420c604b529a9463b11d62e1d7ea1aa32a0e32
Lanslk <wolf810210@hotmail.com>
2024-09-01 22:03:55 +1000

    100% HD

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -8,12 +8,14 @@ public class PrintAndHide : MonoBehaviour
     private int i = 3;
 
     public Renderer rend;
+    private int randomNumber;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log(redObj)
+        randomNumber = Random.Range(150, 251);
+
     }
 
     // Update is called once per frame
@@ -22,5 +24,17 @@ public class PrintAndHide : MonoBehaviour
         i++;
         Debug.Log(gameObject.name + ":" + i);
         
+        // Check if the game object's tag is "Red" and if i = 100
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            // Deactivate the game object
+            gameObject.SetActive(false);
+        }
+        // Check if the game object's tag is "Blue" and if i equals the random number
+        else if (gameObject.tag == "Blue" && i == randomNumber)
+        {
+            // Disable the Renderer component
+            rend.enabled = false;
+        }   
     }
 }
